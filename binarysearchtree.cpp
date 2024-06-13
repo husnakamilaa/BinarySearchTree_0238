e816ded8a48a05ecbde3a36b7a1ca7015544104b
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-06-13 15:38:25 +0700

    mendefinisikan member methode prosedur insert

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -28,6 +28,28 @@ public:
 
     void insert(string element) //Insert a node in the binary search tree
     {
-        
+        Node* newNode = new Node(element, nullptr, nullptr); // Allocate memory for the noew node
+        newNode->info = element; // Assign value to the data field of the new node
+        newNode->leftchild = nullptr; // Make the left child of the new node point to NULL
+        newNode->rightchild = nullptr; // Make the right child of the new node point to NULL
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); // Locate the node which will be the parent of the node to be insert
+
+        if (parent == nullptr) // If the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; // Mark the new node as ROOT
+            return; // Exit
+        }
+
+        if (element < parent->info) // If the value in the data field of the new node is less than that of the parent
+        {
+            parent->leftchild = newNode; // Make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) // If the value in the data field of the new node is greater than that of the parent
+        {
+            parent->rightchild = newNode; // Make the right child of the parent point to the new node
+        }
     }
 };
