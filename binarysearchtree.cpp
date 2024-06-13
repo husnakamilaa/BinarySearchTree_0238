ead37eb1a93db5ca8a1e510cf6ededeae778f69b
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-06-13 15:45:39 +0700

    mendeklarasikan dan mendefinisikan member methode prosedur search

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -52,4 +52,19 @@ public:
             parent->rightchild = newNode; // Make the right child of the parent point to the new node
         }
     }
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+    // this
+    currentNode = ROOT;
+    parent = NULL;
+    while ((currentNode != NULL) && (currentNode->info != element))
+    {
+        parent = currentNode;
+        if (element < currentNode->info)
+            currentNode = currentNode->leftchild;
+        else
+            currentNode = currentNode->rightchild;
+    }
+    }
 };
